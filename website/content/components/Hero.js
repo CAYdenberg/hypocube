6e248687ba720d722228039c64b82c1a58f55cbb
Casey Ydenberg <hi@CAYdenberg.io>
2021-09-28 11:59:43 -0700

    Rewrite the Motivation section of the docs (#80)

---
+++ website/content/components/Hero.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export default styled('p')`
+  font-family: Roboto, sans-serif;
+  font-style: italic;
+  font-size: 1.5rem;
+  color: rgb(59, 69, 78);
+  margin-bottom: 1.5em;
+`;
